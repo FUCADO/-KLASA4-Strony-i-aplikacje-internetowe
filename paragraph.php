6dd923459a2d35fc0786282d03858fd2663bac05
Filip Kwiecień <filipkk31@gmail.com>
2024-09-16 11:06:44 +0200

    ok

---
+++ paragraph.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <style>
+        h3{
+            color: red;
+        }
+        p{
+           color: #336ee1 
+        }
+        div{
+           color: green
+           
+        }
+    </style>
+</head>
+<body>
+    <?php
+    $a = 97;
+    $b = 3.14;
+    $c = 'PHP';
+    ?>
+
+<h3><?php echo $a ?> jest liczbą pierwszą</h3>
+<p>Liczba Pi zaokrąglona do dowóch miejsc po przecinku: <?php echo $b ?></p>
+<div><?php echo $c ?> to język skryptowy wykoany po stronie serwera</div>
+</body>
+</html>
--- zmienna_stale.php
+++ zmienna_stale.php
@@ -9,7 +9,7 @@
     <?php
     $ciag_znakow = "Ala ma kota";
     $x = 50;
-    $liczba_zmien = "10.113";
+    $liczba_zmien = 10.113;
     $boolean = true;
     ?>
 
@@ -17,5 +17,17 @@
 <?php echo "echo dla liczby całkowitej: $x" ?> </br>
 <?php echo "echo dla liczby zmiennnej: $liczba_zmien" ?> </br>
 <?php echo "echo dla boolowskiej: $boolean" ?> </br>
+
+<p>var_dump dla ciągu znaków: <?php var_dump($ciag_znakow)?></p> 
+<p>var_dump dla liczby: <?php var_dump($x)?></p> 
+<p>var_dump dla liczby zmiennej: <?php var_dump($liczba_zmien)?></p> 
+<p>var_dump dla boola: <?php var_dump($boolean)?></p> 
+
+<?php 
+define("pierwsza", "1994");
+define("druga", "19");
+?>
+<p>print dla pierwszej stałej: <?php print pierwsza ?></p>
+<p>print dla drugiej stałej: <?php print druga ?></p>
 </body>
 </html>
