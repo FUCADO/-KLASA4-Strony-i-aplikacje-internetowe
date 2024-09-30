5978f6dcfee06cdf00421cb5af40e540fbd1375d
Filip Kwiecień <filipkk31@gmail.com>
2024-09-30 09:32:30 +0200

    ok

---
+++ php/dzialania_matematyczne.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php 
+    $x=5;
+    $y=15;
+    $a=4;
+    $b=35;
+    $r=16;
+    $alfa=93;
+     ?>
+
+     <fieldset>
+        <legend> AD 1</legend>
+        <p>Wynik dodawania 5 i 15 wynosi: <?php echo 5+15; ?></p>
+        <p>Wynik odejmowania 5 i 15 wynosi: <?php echo 5-15; ?></p>
+        <p>Wynik mnożenia 5 i 15 wynosi: <?php echo 5*15; ?></p>
+        <p>Wynik dzielenia 5 i 15 wynosi: <?php echo 5/15; ?></p>
+        <p>Wynik dzielenie z resztą 5 i 15 wynosi: <?php echo 5%15; ?></p>
+     </fieldset>
+     <fieldset>
+        <legend>AD 2</legend>
+        <p>Pole prostokąta o bokach 4 i 35 wynosi: <?php echo $a*$b; ?> jednostek kwadratowcyh</p>
+     </fieldset>
+     <fieldset>
+        <legend>AD 3</legend>
+        <p>Pole koła o promieniu 16 wynosi: <?php echo $r*$r*3.14; ?> jednostek kwadratowcyh</p>
+     </fieldset>
+</body>
+</html>
---
+++ php/funkcje_liczbowe.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <style>
+        p{
+            color: aliceblue;
+            background: darkblue;
+        }
+    </style>
+</head>
+<body>
+    <p><strong>Funkcja abs: <?php echo abs(-4);?></strong></p>
+    <p><strong>Funkcja min i max: <?php echo min(0, 150, 30, 20, -8, -200);  echo max(0, 150, 30, 20, -8, -200); ?></strong></p>
+    <p><strong>Funkcja pi: <?php echo pi() ?></strong></p>
+    <p><strong>Funkcja round: <?php echo round(3,4);?></br>
+    <?php echo round(3,6);?> </br> 
+    <?php echo round(5,045);?> </br> </strong></p>
+    <p><strong>Funkcja rand: <?php echo rand(900, 1000); ?></strong></p>
+    <p><strong>Funkcja sqrt: <?php echo sqrt(0); ?></strong></p>
+    <p><strong>Funkcja sqrt: <?php echo sqrt(64); ?></strong></p>
+    <p><strong>Funkcja sqrt: <?php echo sqrt(25); ?></strong></p>
+    <p><strong>Funkcja sqrt: <?php echo sqrt(256); ?></strong></p>
+</body>
+</html>
