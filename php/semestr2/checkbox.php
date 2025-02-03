b9d393382124b46d7bc590b47cae2b6c2203e4e5
FUCADO <filipkk31@gmail.com>
2025-02-03 11:11:57 +0100

    ok

---
+++ php/semestr2/checkbox.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <style>
+        form{
+            border: solid 3px blue;
+            padding: 15px;
+            background-color: aliceblue;
+        }
+    </style>
+</head>
+<body>
+<legend>Alfabet</legend>
+    <form method="GET" action="formularz_akcja.html">
+
+<?php $litery = range('a','z');?>
+<?php foreach ($litery as $x) {
+    echo '<input type="checkbox">'. $x . '';
+}?>
+<input id="wyslij" type="submit">
+
+
+</form>
+</body>
+</html>
